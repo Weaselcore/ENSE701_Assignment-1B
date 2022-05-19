867005701e61992bb713cc373229679dee86e79e
Weaselcore <42705062+Weaselcore@users.noreply.github.com>
2022-05-19 18:12:37 +1200

    Fixed uri extraction logic

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const db = config.get('mongoURI');
 
 const connectDB = async() => {
     try {
-        await mongoose.connect(db, {
+        await mongoose.connect(JSON.parse(db), {
                 useNewUrlParser: true
             }
         );
