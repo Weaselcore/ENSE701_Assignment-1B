707f6f96c03bc60d7318f2cce979163501fe29d4
Weaselcore <42705062+Weaselcore@users.noreply.github.com>
2022-05-19 21:03:46 +1200

    Update Articles.js

--- frontend/src/pages/Articles.js
+++ frontend/src/pages/Articles.js
@@ -3,13 +3,12 @@ import axios from 'axios';
 
 function SubmitForm() {
 
-  const result = axios.get('http://localhost:8082/api/articles/');
   return (
     <>
       <h1>Articles</h1>
-      
+
       <ul>
-        {axios.get('http://localhost:8082/api/articles/').then(result.map(e => (<li>{e}</li>)))}
+        {axios.get('http://localhost:8082/api/articles/').then(result => result.map(e => (<li>{e}</li>)))}
       </ul>
     </>
   );
