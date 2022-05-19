29c98e817f4b75fbeacd0ff0b97e6898ae1c5f47
Weaselcore <42705062+Weaselcore@users.noreply.github.com>
2022-05-19 20:51:13 +1200

    Update Articles.js

--- frontend/src/pages/Articles.js
+++ frontend/src/pages/Articles.js
@@ -4,14 +4,9 @@ import axios from 'axios';
 function SubmitForm() {
   <h1>Articles</h1>
 
-  axios
-  .get('http://localhost:8082/api/articles/')
-  .then((data) => {
-    <div>data</div>
-  })
-  .catch(err => {
-    console.log(`Error in CreateBook!: ${err}`);
-  })
+  const result = axios.get('http://localhost:8082/api/articles/');
+
+  <div>{ result }</div>
 };
 
 export default SubmitForm;
