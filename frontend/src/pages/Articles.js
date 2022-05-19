aeb29099afe5c3ed5ccae71298cae61975312f12
Weaselcore <42705062+Weaselcore@users.noreply.github.com>
2022-05-19 21:13:08 +1200

    Added state for articles page

--- frontend/src/pages/Articles.js
+++ frontend/src/pages/Articles.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function SubmitForm() {
+
+  const [articles ,setArticles] = useState()
+  useEffect(() => {
+    getArticles();
+  }, [])
+
+  const getArticles = async() => {
+    const data = await axios.get('http://localhost:8082/api/articles/');
+    setArticles(data);
+  };
 
   return (
     <>
       <h1>Articles</h1>
 
       <ul>
-        {axios.get('http://localhost:8082/api/articles/').then(result => result.map(e => (<li>{e}</li>)))}
+        {articles.map(e => (<li>{e}</li>))}
       </ul>
     </>
   );
