3e68b14707cc3539af16fd751b2cf34a130e5953
Weaselcore <42705062+Weaselcore@users.noreply.github.com>
2022-05-19 18:45:16 +1200

    Added middleware

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require("path");
 //var cors = require('cors');
 // routes
 const articles = require('./routes/api/article')
@@ -15,6 +16,13 @@ app.use(express.json());
 app.use('/api/articles', articles);
 
 const port = process.env.PORT || 8082;
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./frontend/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./frontend/build", "index.html"));
+});
 
 // if(process.env.NODE_ENV === 'production'){
 //     app.use(express.static('_____'));
