7c5dfbcb1d5fd0810a3989d40185078527be3085
Weaselcore <42705062+Weaselcore@users.noreply.github.com>
2022-06-06 21:20:53 +1200

    Clean up routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,30 +16,18 @@ function App() {
     <Router>
         <ul className="header">
           <li><NavLink exact="true" to="/">Home</NavLink></li>
-          <li><NavLink to="/users">Users</NavLink></li>
-          <li><NavLink to="/articles">Articles</NavLink></li>
           <li><NavLink to="/submitarticle">Submit Article</NavLink></li>
         </ul>
 
         <div className="content">
           <Routes>
-            <Route path="/" element={<Home />}/>
-            <Route path="/users" element={<Users />}/>
-            <Route path="/articles" element={<Articles/>}/>
+            <Route path="/" element={<Articles/>}/>
             <Route path="/submitarticle" element={<SubmitForm/>}/>
             <Route path="/*" element={<NotFoundPage/>}/>
           </Routes>
         </div>
     </Router>
   );
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
 }
 
 function NotFoundPage() {
--- frontend/src/components/TableSetUp.js
+++ frontend/src/components/TableSetUp.js
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React from "react";
 import { useTable, useSortBy, usePagination, useFilters } from 'react-table';
 
 const TableSetUp = ({ columns, data }) => {
